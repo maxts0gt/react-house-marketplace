1defadbff925e2352fb8075ba26e80e873e1eb31
Max <bazarsadtsogtjargal@gmail.com>
2022-01-11 22:03:01 +0900

    add Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
-import Profile from './pages/Profile';
+// import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -19,8 +20,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
   );
 }
